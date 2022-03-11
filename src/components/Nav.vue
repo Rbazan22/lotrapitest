b8a8788c11e5aae206ee66db98cef2ed00e3b318
Rbazan22 <73728631+Rbazan22@users.noreply.github.com>
2022-03-11 17:53:56 -0600

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="bg-blue-800 text-blue-200">
+    <div class="container mx-auto flex items-center justify-center">
+      <h1 class="py-8 px-2 tracking-tighter text-3xl font-thin">
+        Advice<span class="font-normal">Search</span>
+      </h1>
+    </div>
+  </div>
+</template>
